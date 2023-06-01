427062b0f7c95b8de25651693a8cee422f97c053
Julia Chan <60370842+jchanDev@users.noreply.github.com>
2023-06-01 14:11:52 -0700

    Update ucsbece152a_fsm.sv

--- ucsbece152a_fsm.sv
+++ ucsbece152a_fsm.sv
@@ -5,16 +5,6 @@
 * File: ucsbece152a_fsm.sv
 * Description: Starter code for fsm.
 */
-typedef enum logic [2:0] {
-   S000_000,
-   S000_100,
-   S000_110,
-   S000_111,
-   S001_000,
-   S011_000,
-   S111_000,
-   S111_111
-} state_t;
 module ucsbece152a_fsm (
    input logic clk,
    input logic rst_n,
@@ -25,8 +15,7 @@ module ucsbece152a_fsm (
 
    output state_t state_o,
    output logic [5:0] pattern_o
-);
-//import taillights_pkg::*;
+);import taillights_pkg::*;
 state_t state_d, state_q = S000_000;
 assign state_o = state_q;
 // TODO: Implement the FSM and drive `pattern_o`
